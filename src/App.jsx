fdef7ff91536a48b8394cd9d06e92484e26dd0e6
shivank <shivankkunwar100@gmail.com>
2023-12-03 12:38:05 +0530

    added selection checkbox

--- src/App.jsx
+++ src/App.jsx
@@ -61,11 +61,13 @@ function App() {
       <div className="table-responsive p-5 bg-gray-100 rounded-xl">
         <table className="w-full table-auto">
           <thead className="bg-gray-50 border-b-2 border-gray-200 text-left ">
+
             <tr>
+              <th className='p-3 w-12 mr-5'><input id="checkbox-all-search" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/></th>
               <th className="w-45 p-3 text-sm font-semibold tracking-wide text-left ">
                 Name
               </th>
-              <th className="w-96 p-3 text-sm font-semibold tracking-wide text-left">
+              <th className="w-72 p-3 text-sm font-semibold tracking-wide text-left">
                 Email
               </th>
               <th className="w-56 p-3 text-sm font-semibold tracking-wide text-left">
